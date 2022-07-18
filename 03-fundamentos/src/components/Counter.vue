2172728314aa9185e14af66e939386efcd7d9d66
maproderm <maprodermweb@gmail.com>
2022-07-18 17:47:55 -0500

    componentes

---
+++ 03-fundamentos/src/components/Counter.vue
@@ -0,0 +1,31 @@
+<template>
+    <h2>Counter</h2>
+    <p>{{ counter }} <sup>2</sup> = {{ counter * counter }} </p>
+    <p>{{ counter }} <sup>2</sup> = {{ getSquareValue() }} </p>
+    <p>{{ counter }} <sup>2</sup> = {{ squareCounter }} </p>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            counter:5
+        }
+    },
+    methods : {
+        getSquareValue(){
+            return this.counter * this.counter
+        }
+    },
+    computed: {
+        squareCounter(){
+            return this.counter * this.counter
+        }
+    }
+}
+</script>
+
+    
+<style>
+
+</style>
